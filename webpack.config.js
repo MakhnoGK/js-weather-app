9aadfafab8dfd2122d1ee6eda03e9c756e231e7e
Hryhorii Khyzhniakov <soults15@gmail.com>
2020-10-28 09:49:34 +0200

    (F) Font loading from SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -2,70 +2,96 @@ const path = require('path')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-  entry: {
-    main: './src/app.js',
-    normalize: './node_modules/normalize.css/normalize.css'
-  },
+const settings = (env) => {
+  return {
+    entry: {
+      main: './src/app.js'
+    },
 
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist'
-  },
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: './dist'
+    },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ],
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        minify: false
+      })
+    ],
 
-  module: {
-    rules: [
-      /*
-       * For using with just CSS
-       * */
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      },
+    module: {
+      rules: [
+        /*
+         * For using with just CSS
+         * */
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
 
-      /*
-       * And for use with SASS pre-processor (optionally)
-       * */
-      {
-        test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },
+        /*
+         * And for use with SASS pre-processor (optionally)
+         * */
+        {
+          test: /\.s[ac]ss$/i,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        },
 
-      /*
-       * Loading image files in specific directories
-       */
-      {
-        test: /\.(png|gif|svg|jpe?g)$/i,
-        loader: 'file-loader',
-        options: {
-          name (resourcePath, resourceQuery) {
-            if (process.env.NODE_ENV === 'development') {
-              return '[path][name].[ext]'
-            }
+        /*
+         * Loading image files in specific directories
+         */
+        {
+          test: /\.(png|gif|svg|jpe?g)$/i,
+          loader: 'file-loader',
+          options: {
+            name (resourcePath, resourceQuery) {
+              if (env.production) {
+                return '[name].[ext]'
+              }
 
-            return '[contenthash].[ext]'
-          },
-          outputPath: 'images'
+              return '[contenthash].[ext]'
+            },
+            outputPath: 'images'
+          }
+        },
+
+        /*
+         * Loading fonts
+         */
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          loader: 'file-loader',
+          options: {
+            name (resourcePath, resourceQuery) {
+              if (env.production) {
+                return '[name].[ext]'
+              }
+
+              return '[contenthash].[ext]'
+            },
+            outputPath: 'fonts'
+          }
+        },
+
+        /*
+         * Fix for HtmlWebpackPlugin can't load images from template
+         */
+        {
+          test: /\.(html)$/i,
+          loader: 'html-loader',
+          options: {
+            minimize: false
+          }
         }
-      },
-      {
-        test: /\.(html)$/i,
-        loader: 'html-loader',
-        options: {
-          minimize: false
-        }
-      }
-    ]
-  },
+      ]
+    },
 
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist')
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist')
+    }
   }
 }
+
+module.exports = settings
