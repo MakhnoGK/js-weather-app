b75b6253b89dc07f4080e6048855cdd411d2284e
Hryhorii Khyzhniakov <soults15@gmail.com>
2020-10-28 08:41:48 +0200

    (F) Eslint (Eslint, prettier and VS Code plugins)

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,40 @@
-const path = require('path');
+const path = require('path')
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/app.js',
+  entry: {
+    main: './src/app.js',
+    normalize: './node_modules/normalize.css/normalize.css'
+  },
 
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist',
+    contentBase: './dist'
   },
 
   plugins: [
-    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
   ],
 
   module: {
     rules: [
       /*
-      * For using with just CSS
-      * */
+       * For using with just CSS
+       * */
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader']
       },
 
       /*
-      * And for use with SASS pre-processor (optionally)
-      * */
+       * And for use with SASS pre-processor (optionally)
+       * */
       {
         test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ['style-loader', 'css-loader', 'sass-loader']
       },
 
       /*
@@ -39,25 +44,28 @@ module.exports = {
         test: /\.(png|gif|svg|jpe?g)$/i,
         loader: 'file-loader',
         options: {
-          name(resourcePath, resourceQuery) {
+          name (resourcePath, resourceQuery) {
             if (process.env.NODE_ENV === 'development') {
-              return "[path][name].[ext]";
+              return '[path][name].[ext]'
             }
 
-            return '[contenthash].[ext]';
+            return '[contenthash].[ext]'
           },
-          outputPath: 'images',
-        },
+          outputPath: 'images'
+        }
       },
       {
         test: /\.(html)$/i,
-        use: ['html-loader']
+        loader: 'html-loader',
+        options: {
+          minimize: false
+        }
       }
-    ],
+    ]
   },
 
   output: {
-    filename: 'script.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
